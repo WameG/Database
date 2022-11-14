7707cf65d0cc17f55812bd4a9f182c1a7c393892
WameG <wgassama@yahoo.dk>
2022-11-14 21:43:56 +0100

    Update SQL Script

--- Database (Obligatorisk)/SQLQuery1.sql
+++ Database (Obligatorisk)/SQLQuery1.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE Facility (
     Facility_No INT NOT NULL,
     Name varchar(50) NOT NULL,
-    PRIMARY KEY (Facility_No)
+    PRIMARY KEY (Facility_No),
+    UNIQUE (Name)
 );
 
 INSERT INTO Facility(Facility_No, Name)
@@ -13,10 +14,11 @@ CREATE TABLE Hotel_Facility (
     Hotel_No INT NOT NULL,
     PRIMARY KEY (HotelFacility_No),
     FOREIGN KEY (Facility_No) REFERENCES Facility(Facility_No),
-    FOREIGN KEY (Hotel_No) REFERENCES Hotel(Hotel_No)
+    FOREIGN KEY (Hotel_No) REFERENCES Hotel(Hotel_No),
+    CONSTRAINT UC_Hotel_Facility UNIQUE (Facility_No,Hotel_No)
 );
 
 INSERT INTO Hotel_Facility(HotelFacility_No, Facility_No, Hotel_No)
-VALUES(1,1,1)
+VALUES(1,1,3)
 
 
